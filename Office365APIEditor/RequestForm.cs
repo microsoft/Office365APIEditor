32457864a4c5b3cf1c9bb10c6b5c06653a589670
Ryutaro Koma <rykoma@microsoft.com>
2016-04-20 19:21:17 +0900

    v0.3.0.1

--- Office365APIEditor/RequestForm.cs
+++ Office365APIEditor/RequestForm.cs
@@ -154,6 +154,7 @@ namespace Office365APIEditor
 
                 // Parse the JSON data.
                 // Then, decode double byte characters.
+                // TODO: add switch.
                 textBox_Result.Text += DecodeJsonResponse(parseJson(jsonResponse));
 
                 // Save application setting.
