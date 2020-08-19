6c2e2f8687c30e4d057a4a34fbd267f82d444d78
rykoma <rykoma@microsoft.com>
2020-08-19 22:23:19 +0900

    Update PublishSite pre build task Setp 2

--- PublishSite/PreBuild.ps1
+++ PublishSite/PreBuild.ps1
@@ -1,21 +1,37 @@
 param(
-    [string]$MsiPath,
-    [string]$VersionOutPath,
-    [string]$ReleaseFolder,
-    [string]$ZipOutDir
+    [string]$wwwrootPath
 )
 
+$InstallersPath = Join-Path $wwwrootPath "installers"
+
+$MsiFile = Get-ChildItem -Path $InstallersPath -Filter *.msi
+
+if ($MsiFile.GetType().Name -ne "FileInfo") {
+	# 2 or more MSI files are existing.
+	exit 1
+}
+
+Rename-Item $MsiFile.FullName -NewName "Setup.msi"
+
+$MsiFile = Get-ChildItem -Path $InstallersPath -Filter *.msi
+
+$ZipFile = Get-ChildItem -Path $InstallersPath -Filter *.zip
+
+if ($ZipFile.GetType().Name -ne "FileInfo") {
+	# 2 or more ZIP files are existing.
+	exit 2
+}
+
+Rename-Item $ZipFile.FullName -NewName "Office365APIEditor.zip"
+
+$ZipFile = Get-ChildItem -Path $InstallersPath -Filter *.zip
+
+$VersionOutPath = Join-Path $wwwrootPath "latestMsi.txt"
 $Installer = New-Object -ComObject "WindowsInstaller.Installer"
-$Database = $Installer.OpenDatabase($MsiPath, 0)
+$Database = $Installer.OpenDatabase($MsiFile.FullName, 0)
 $View = $Database.OpenView("SELECT Value FROM Property WHERE Property='ProductVersion'")
 $View.Execute()
 $Result = $View.Fetch()
 $Version = $Result.StringData(1)
 
 $Version | Out-File -FilePath $VersionOutPath -Encoding UTF8
-
-$ZipFileName = "Office365APIEditor.zip"
-
-$ZipFilePath = Join-Path $ZipOutDir $ZipFileName
-
-Compress-Archive -Path:("$ReleaseFolder\*") -DestinationPath:$ZipFilePath -Force
