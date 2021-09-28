7d1500d52b499496b544b4b5dba2dce2fa0bb4eb
rykoma <rykoma@microsoft.com>
2021-09-28 23:43:08 +0900

    Update PublishSite project Changed the target framework to .NET Core 3.1

--- PublishSite/Startup.cs
+++ PublishSite/Startup.cs
@@ -4,6 +4,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace PublishSite
 {
@@ -13,11 +14,11 @@ namespace PublishSite
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc(options => options.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
